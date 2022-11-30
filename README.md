0e92a662edea52102c611eed650a8bf12f35a3b3
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-11-30 23:15:55 +0700

    Make It Customizable

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ Mention Extension for [Mecha](https://github.com/mecha-cms/mecha)
 =================================================================
 
 ![Code Size](https://img.shields.io/github/languages/code-size/mecha-cms/x.mention?color=%23444&style=for-the-badge)
-
-Mention users in page content by its user name.
--- index.php
+++ index.php
@@ -1,42 +1,60 @@
 <?php namespace x;
 
 function mention($content) {
-    if ($content && \is_string($content) && false !== \strpos($content, '@')) {
-        $out = "";
-        $parts = \preg_split('/(<!--[\s\S]*?-->|' . (static function ($tags) {
-            foreach ($tags as &$tag) {
-                $tag = '<' . $tag . '(?:\s(?:"[^"]*"|\'[^\']*\'|[^\/>])*)?>[\s\S]*?<\/' . $tag . '>';
-            }
-            unset($tag);
-            return \implode('|', $tags);
-        })([
-            'pre',
-            'code', // Must come after `pre`
-            'kbd',
-            'script',
-            'style',
-            'textarea'
-        ]) . '|<[^>]+>)/i', $content, -1, \PREG_SPLIT_DELIM_CAPTURE | \PREG_SPLIT_NO_EMPTY);
-        foreach ($parts as $v) {
-            if (0 === \strpos($v, '<') && '>' === \substr($v, -1)) {
-                $out .= $v; // Is a HTML tag
-            } else {
-                $out .= false !== \strpos($v, '@') ? \preg_replace_callback('/(?<=\W|^)@[a-z\d-]+/', static function ($m) {
-                    if (\is_file($file = \LOT . \D . 'user' . \D . \substr($m[0], 1) . '.page')) {
-                        $user = new \User($file);
-                        return '<a href="' . $user->url . '" target="_blank" title="' . $user->user . '">' . $user . '</a>';
-                    }
-                    return $m[0];
-                }, $v) : $v; // Is a plain text
-            }
-        }
-        return $out;
+    if (!$content || !\is_string($content)) {
+        return $content;
     }
-    return $content;
+    if (false === \strpos($content, '@')) {
+        return $content;
+    }
+    $out = "";
+    $parts = \preg_split('/(<!--[\s\S]*?-->|' . (static function ($tags) {
+        foreach ($tags as &$tag) {
+            $tag = '<' . $tag . '(?:\s(?:"[^"]*"|\'[^\']*\'|[^\/>])*)?>[\s\S]*?<\/' . $tag . '>';
+        }
+        unset($tag);
+        return \implode('|', $tags);
+    })([
+        'pre',
+        'code', // Must come after `pre`
+        'kbd',
+        'script',
+        'style',
+        'textarea'
+    ]) . '|<[^>]+>)/i', $content, -1, \PREG_SPLIT_DELIM_CAPTURE | \PREG_SPLIT_NO_EMPTY);
+    $key = \State::get('x.mention.key') ?? 'user';
+    foreach ($parts as $v) {
+        if (0 === \strpos($v, '<') && '>' === \substr($v, -1)) {
+            $out .= $v; // Is a HTML tag
+        } else {
+            $out .= false !== \strpos($v, '@') ? \preg_replace_callback('/(?<=\W|^)@[a-z\d-]+/', static function ($m) use ($key) {
+                if (\is_file($file = \LOT . \D . 'user' . \D . \substr($m[0], 1) . '.page')) {
+                    $user = new \User($file);
+                    if ('author' === $key) {
+                        return '<a href="' . ($user->link ?? $user->url) . '" target="_blank" title="' . $user->user . '">' . $user . '</a>';
+                    }
+                    if ('user' === $key) {
+                        return '<a href="' . ($user->link ?? $user->url) . '" target="_blank" title="' . $user . '">' . $user->user . '</a>';
+                    }
+                    if (\is_callable($key)) {
+                        // Prioritize `$key` as a property name over as a function name. If `$key` is a function name in
+                        // the form of a string, make sure that `$user->{$key}` is `null` before treating `$key` as a
+                        // function name to be called later
+                        if (\is_string($key) && null !== ($v = $user->{$key})) {
+                            return '<a href="' . ($user->link ?? $user->url) . '" target="_blank" title="' . $user->user . '">' . $v . '</a>';
+                        }
+                        return \fire($key, [$m[0]], $user);
+                    }
+                    return '<a href="' . ($user->link ?? $user->url) . '" target="_blank" title="' . $user->user . '">' . ($user->{$key} ?? $user) . '</a>';
+                }
+                return $m[0];
+            }, $v) : $v; // Is a plain text
+        }
+    }
+    return $out;
 }
 
 \Hook::set([
     'page.content',
-    'page.description',
-    'page.title'
+    'page.description'
 ], __NAMESPACE__ . "\\mention", 2);
